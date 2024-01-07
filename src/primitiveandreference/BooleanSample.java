1404d375b617e2af4a39e53b7d91e8552ba3bbb1
wild-mental <wild.mental@kidsnote.com>
2024-01-07 19:12:08 +0900

    - 기본자료형과 참조자료형 코드샘플 추가 2

---
+++ src/primitiveandreference/BooleanSample.java
@@ -0,0 +1,12 @@
+package primitiveandreference;
+
+public class BooleanSample {
+    public static void main(String[] args) {
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+        myTrue = 1 == 1;
+        myFalse = 1 == 0;
+
+    }
+}
