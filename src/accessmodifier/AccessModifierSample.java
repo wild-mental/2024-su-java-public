bd6266156a5da8abe15740d310bd9ab99143eb4a
wild-mental <wild.mental@kidsnote.com>
2024-01-07 19:19:19 +0900

    - 접근제어자 불필요한 샘플코드 삭제

--- src/accessmodifier/AccessModifierSample.java
+++ src/accessmodifier/AccessModifierSample.java
@@ -6,17 +6,6 @@ import accessmodifier.b.C;
 
 public class AccessModifierSample {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
         System.out.println("-----------   Direct Call of A methods    -----------");
 //        A.printPrivateStrA();
 //        A.printProtectedStrA();
