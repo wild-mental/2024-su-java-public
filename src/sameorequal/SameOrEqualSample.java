d759a430ba06b5a9e1dca45527c9631985de1cf2
wild-mental <wild.mental@kidsnote.com>
2024-01-07 19:14:39 +0900

    - 동일 객체 vs 동일 값 비교 주석 추가

--- src/sameorequal/SameOrEqualSample.java
+++ src/sameorequal/SameOrEqualSample.java
@@ -3,7 +3,7 @@ package sameorequal;
 public class SameOrEqualSample {
     public static void main(String[] args) {
         String a = "a";
-        if (a == "a") {
+        if (a == "a") {  // hashCode() - 이후 객체의 동일성 여부를 비교하기 위한 용도로 사용하게 된다.
             System.out.println("same!");
         }
 
