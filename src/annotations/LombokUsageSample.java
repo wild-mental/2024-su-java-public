06d85621aeb8ecf22978c61b157bc29b708f956e
wild-mental <wild.mental@kidsnote.com>
2024-01-09 23:06:11 +0900

    1/8주 강의 샘플 코드 추가 (8) - lombok 코드 수정

--- src/annotations/LombokUsageSample.java
+++ src/annotations/LombokUsageSample.java
@@ -4,20 +4,10 @@ import lombok.Data;
 
 @Data
 public class LombokUsageSample {
-    private boolean employed = false;
     private int age = 20;
+    private boolean employed = false;
 
-    public static void main(String[] args) {
-        LombokUsageSample sul = new LombokUsageSample();
-        System.out.println(sul.getAge());
-        System.out.println(sul.isEmployed());
-        sul.setAge(30);
-        sul.setEmployed(true);
-        System.out.println(sul.getAge());
-        System.out.println(sul.isEmployed());
-    }
-
-    /* lombok makes these with just simple annotations
+    /* lombok 을 사용하면 아래와 같은 메서드를 수동 생성할 필요가 없어짐
     public void setEmployed(boolean employed) {
         this.employed = employed;
     }
@@ -26,4 +16,15 @@ public class LombokUsageSample {
         return employed;
     }
     */
+
+    public static void main(String[] args) {
+        LombokUsageSample sul = new LombokUsageSample();
+        System.out.println(sul.getAge());
+        sul.setAge(30);
+        System.out.println(sul.getAge());
+
+        System.out.println(sul.isEmployed());
+        sul.setEmployed(true);
+        System.out.println(sul.isEmployed());
+    }
 }
