e8a6c19d239bdaec88057d385a703602107442b5
wild-mental <wild.mental@kidsnote.com>
2024-01-25 02:40:01 +0900

    1/22주 강의 코드 추가 - 멀티스레딩 동시성 기초 문제 solution 코드 보강

--- src/multithreading/ConcurrencyBasicSolutionSample.java
+++ src/multithreading/ConcurrencyBasicSolutionSample.java
@@ -14,10 +14,30 @@ public class ConcurrencyBasicSolutionSample {
         }
     }
 
-    private void process() {
-        lock.lock(); // lock 을 통해 스레드가 하나씩만 통과할 수 있는 임계영역 생성
+    private synchronized void process() {  // 메서드에 synchronized 키워드를 사용해 스레드 동기화
         if (passCount < 10) {
-            // 아래 작업들은 임계영역에서 이루어지므로 스레드 안전
+            try { Thread.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+            System.out.println("passCount: " + (++passCount));
+        } else {
+            System.err.println("blocked by passCount condition");
+        }
+    }
+
+    private void process2() {
+        synchronized (this) {  // synchronized 블록을 사용해 스레드 동기화
+            if (passCount < 10) {
+                try { Thread.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+                System.out.println("passCount: " + (++passCount));
+            } else {
+                System.err.println("blocked by passCount condition");
+            }
+        }
+    }
+
+    private void process3() {
+        lock.lock(); // lock 을 통해 스레드가 하나씩만 통과할 수 있는 임계영역 생성
+        // 아래 작업들은 임계영역에서 이루어지므로 스레드 안전
+        if (passCount < 10) {
             try { Thread.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("passCount: " + (++passCount));
             lock.unlock();  // 중요 작업을 마친 경우 임계영역 해제
