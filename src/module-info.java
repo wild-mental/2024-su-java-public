c53650768bbb957431c4184a0e009c3a9974e89d
wild-mental <wild.mental@kidsnote.com>
2024-01-24 11:44:39 +0900

    1/22주 강의 코드 추가 - module-info.java 파일 추가

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module sample_java_basic_2024_ {
+    requires java.base; // java.base 모듈에 의존
+    requires lombok;
+    requires java.desktop;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires jdk.unsupported;
+}
