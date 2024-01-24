404238a6d1e071d5589210fcb73492c4c5bd4e75
wild-mental <wild.mental@kidsnote.com>
2024-01-25 02:57:15 +0900

    1/22주 강의 코드 추가 - 멀티스레딩 동시성 코어 점유 및 elapsed 타임 예제 보완

--- src/multithreading/MultiThreadingSample.java
+++ src/multithreading/MultiThreadingSample.java
@@ -7,50 +7,13 @@ public class MultiThreadingSample {
         int reps = 100;
         int delayTime = 10;
         Toolkit tk = Toolkit.getDefaultToolkit();
-        // beepThread 선언
-        // (1) 익명 자식객체를 구현하는 Thread 생성 방식
-        // Thread 클래스를 인라인으로 재정의한 익명클래스의 객체를 생성한다.
-        Thread beepThread = new Thread() {
-            @Override
-            public void run () {
-//                long startTime = System.currentTimeMillis();
-                for (int i=0; i<reps; i++) {
-                    tk.beep();
-                    // delay 를 위해 스레드 sleep
-                    try { Thread.sleep(delayTime); } catch (InterruptedException e) { throw new RuntimeException(e); }
-                }
-//                long elapsedTime = System.currentTimeMillis() - startTime;
-//                System.out.println("beeping elapsed time: "+elapsedTime+"ms");
-            }
-        };
-        // printThread 선언
-        // (2) 람다식을 사용한 익명 구현객체를 Thread 생성자에 전달하는 방식
-        // 람다 표현식으로 익명 구현 클래스의 객체를 생성해 Runnable 인터페이스를 받는 Thread 클래스의 생성자에 전달한다.
-        Thread printThread = new Thread(() -> {
-//            long startTime = System.currentTimeMillis();
-            for (int i=0; i<reps; i++) {
-                System.out.println("Beep"+i);
-                // delay 를 위해 스레드 sleep
-                try { Thread.sleep(delayTime); } catch (InterruptedException e) { throw new RuntimeException(e); }
-            }
-//            long elapsedTime = System.currentTimeMillis() - startTime;
-//            System.out.println("printing elapsed time: "+elapsedTime+"ms");
-        });
-        //
-//        beepThread.setDaemon(true);
-//        printThread.setDaemon(true);
 
-        // 두 스레드 실행
-//        printThread.start();
-//        beepThread.start();
-
-        // 두 스레드 실행 (with or without ElapsedTime)
-        boolean occupyCpuThread = false;  // true/false 바꿔가며 실험해 보기
-        printThreadWithElapsed(reps, delayTime, occupyCpuThread);
-        beepThreadWithElapsed(tk, reps, delayTime, occupyCpuThread);
+        // 두 스레드 실행 (sleep or occupy )
+        boolean occupyCpu = true;  // true/false 바꿔가며 실험해 보기
+        printThreadWithElapsed(reps, delayTime, occupyCpu);
+        beepThreadWithElapsed(tk, reps, delayTime, occupyCpu);
     }
 
-    // Java 스럽게 만들기
     public static class ThreadWithElapsed extends Thread {
         public  ThreadWithElapsed(Runnable target) {
             super(target);
@@ -69,9 +32,8 @@ public class MultiThreadingSample {
                 System.out.println("Beep" + i);
                 delayMethod(delayTime, isOccupyThread);
             }
-        }
-        );
-        printElapsedThread.setName("printElapsedThread");
+        });
+        printElapsedThread.setName("프린팅 스레드");
         printElapsedThread.start();
     }
     public static void beepThreadWithElapsed(Toolkit tk, int reps, int delayTime, boolean isOccupyThread) {
@@ -80,22 +42,22 @@ public class MultiThreadingSample {
                 tk.beep();
                 delayMethod(delayTime, isOccupyThread);
             }
-        }
-        );
-        beepElapsedThread.setName("beepElapsedThread");
+        });
+        beepElapsedThread.setName("비프음 스레드");
         beepElapsedThread.start();
     }
     public static void delayMethod(int delayTime, boolean isOccupyThread) {
-        if(isOccupyThread) {
-            // delay 방식을 sleep 형에서 CPU 점유형으로 변경
+        if(!isOccupyThread) {
+            // sleep()을 사용한 delay
+            try { Thread.sleep(delayTime); } catch (InterruptedException e) { throw new RuntimeException(e); }
+        } else {
+            // CPU 점유형 delay
             long startTime = System.currentTimeMillis();
             long elapsedTime = 0;
             while (elapsedTime < delayTime) {
                 // Update elapsed time
                 elapsedTime = System.currentTimeMillis() - startTime;
             }
-        } else {
-            try { Thread.sleep(delayTime); } catch (InterruptedException e) { throw new RuntimeException(e); }
         }
     }
 }
