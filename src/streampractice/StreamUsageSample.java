e335b626384c59e20b9f461c0ba810990c1452bb
wild-mental <wild.mental@kidsnote.com>
2024-01-25 01:32:56 +0900

    1/22주 강의 코드 추가 - 스트림 문법 사용 가이드 코드 보강 - Stream 타입 비교 코드 추가 - 함수형 인터페이스 Predicate 타입 비교 추가

--- src/streampractice/StreamUsageSample.java
+++ src/streampractice/StreamUsageSample.java
@@ -1,9 +1,10 @@
 package streampractice;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import pokemoncharactergame.PokemonStruct;
+
+import java.util.*;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -12,9 +13,22 @@ public class StreamUsageSample {
     public static void main(String[] args) {
         int[] values = { -1, -2, -3, -4, -5, 0, 1, 2, 3, 4, 5 };
         IntStream intStream = Arrays.stream(values);
-        int naturalNumCnt = (int)intStream.filter(x -> x > 0).count();
-        System.out.println(naturalNumCnt);
+        // 일괄적으로 메서드를 (클래스 단위가 아니라) 동작 단위로 핸들링 한다.
+        IntPredicate a2 = x ->  x > 0;
+        int naturalNumCnt = (int)intStream.filter(a2).count();
 
+        List<Integer> valuesList = Arrays.asList( -1, -2, -3, -4, -5, 0, 1, 2, 3, 4, 5 );
+        Stream<Integer> integerStream2 = valuesList.stream();
+        Predicate<Integer> a1 = x ->  x > 0;
+        Stream<Integer> integerStream2_1 = integerStream2.filter(a1);
+
+        List<PokemonStruct> pokemonList = new ArrayList<>();
+        pokemonList.add(new PokemonStruct("myPokemon", 1, "mySkill", 1));
+        Stream<PokemonStruct> pokemonStream = pokemonList.stream();
+        Predicate<PokemonStruct> pokemonStructPredicate = pokemonStruct -> pokemonStruct.getHp() > 0;
+        Stream<PokemonStruct> filteredPokemon = pokemonStream.filter(pokemonStructPredicate);
+
+        System.out.println(naturalNumCnt);
         List<Integer> numbers = Arrays.asList( -10, -20, -30, -40, -50, 0, 10, 20, 30, 40, 50 );
         Stream<Integer> integerStream = numbers.stream();
         List<Integer> negativeNumList = integerStream.filter(x -> x < 0).collect(Collectors.toList());
